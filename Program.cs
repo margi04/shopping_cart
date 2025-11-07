8653f454a7473a59070d444df3b1537601ad598f
Vagh Margi <margi.vagh@giriraj.digital.com>
2025-11-07 14:37:50 +0530

    initial commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Shopping_cart
+{
+    static void main()
+    {
+        Dictionary<string, double> productprice = new Dictionary<string, double>()
+        {
+            {"Apple", 0.99},
+            {"Banana", 0.59},
+            {"Orange", 0.79 },
+            {"milk",2.0 }
+        };
+
+        List<string> cart=new List<string>();
+        
+    }
+}
